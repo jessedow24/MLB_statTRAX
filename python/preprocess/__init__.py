a441777be2c3a744d4286cd2df5af6cf9357d432
Jesse <jesse.downing@level3.com>
2019-08-08 19:33:34 -0600

    set_dates function is incorporated and working, can use to easily retrieve new dates

--- python/preprocess/__init__.py
+++ python/preprocess/__init__.py
@@ -9,8 +9,13 @@ save_obj = model.StoreData()
 path = save_obj.get_path()
 read_obj = model.ReadData(path)
 read_obj.set_raw_stats_df()
+read_obj.set_dates()
 read_obj.update_raw_stats_df()
 save_obj.save(read_obj.get_raw_stats_df())
+
+
+
+    
 
    
 
